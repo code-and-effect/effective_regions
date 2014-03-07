c10f397fec490d5fef8742ac2dace0e5deb115dd
Matt Riemer <matthew@agilestyle.com>
2014-03-06 22:05:49 -0700

    refactor cont

--- app/controllers/effective/regions_controller.rb
+++ app/controllers/effective/regions_controller.rb
@@ -12,35 +12,35 @@ module Effective
     def update
       region_params = assign_unique_snippet_ids(params[:content])
 
-      Effective::Region.transaction do
-        regionables = [] # ActiveRecord object that declare acts_as_regionable
+      to_save = [] # ActiveRecord object that declare acts_as_regionable or Effective::Region objects (also ActiveRecords)
 
-        region_params.each do |title, vals|
-          if vals.key?(:data) && vals[:data].key?(:regionable_type) && vals[:data].key?(:regionable_id)
-            regionable = (vals[:data][:regionable_type].safe_constantize).find(vals[:data][:regionable_id]) rescue nil
+      region_params.each do |title, vals|
+        if vals.key?(:data) && vals[:data].key?(:regionable_type) && vals[:data].key?(:regionable_id)
+          regionable = (vals[:data][:regionable_type].safe_constantize).find(vals[:data][:regionable_id]) rescue nil
 
-            if regionable
-              EffectiveRegions.authorized?(self, :update, regionable) # can I update the regionable object?
+          if regionable
+            EffectiveRegions.authorized?(self, :update, regionable) # can I update the regionable object?
 
-              region = regionable.regions.find { |region| region.title == vals[:data][:title] } 
-              region ||= regionable.regions.build(:title => vals[:data][:title])
+            region = regionable.regions.find { |region| region.title == vals[:data][:title] } 
+            region ||= regionable.regions.build(:title => vals[:data][:title])
 
-              regionables << regionable
-            end
-          else
-            region = Effective::Region.global.where(:title => title).first_or_initialize
-            EffectiveRegions.authorized?(self, :update, region) # can I update the global region?
+            to_save << regionable
           end
+        else
+          region = Effective::Region.global.where(:title => title).first_or_initialize
+          EffectiveRegions.authorized?(self, :update, region) # can I update the global region?
 
-          region.content = cleanup(vals[:value])
-
-          region.snippets.clear
-          (vals[:snippets] || []).each { |snippet, vals| region.snippets[snippet] = vals }
-
-          region.save! if region.global? # If it's a global one, save it straight out
+          to_save << region
         end
 
-        regionables.uniq.each { |obj| obj.save! } # We're relying on has_many :autosave => true to save our region objects
+        region.content = cleanup(vals[:value])
+
+        region.snippets.clear
+        (vals[:snippets] || []).each { |snippet, vals| region.snippets[snippet] = vals }
+      end
+
+      Effective::Region.transaction do
+        to_save.uniq.each { |obj| obj.save! } # We're relying on has_many :autosave => true to save our region objects
 
         render :text => '', :status => 200
         return
@@ -73,8 +73,8 @@ module Effective
     def cleanup(str)
       if str
         # Remove the following markup
-        #<div data-snippet="snippet_0" class="text_field_tag-snippet">[snippet_0/1]</div>
-        # And replace with [snippet_0/1]
+        #<div data-snippet="snippet_0" class="text_field_tag-snippet">[snippet_01]</div>
+        # And replace with [snippet_01]
         # So we don't have a wrapping div in our final content
         str.scan(/(<div.+?>)(\[snippet_\d+\])(<\/div>)/).each do |match|
           str.gsub!(match.join(), match[1]) if match.length == 3
