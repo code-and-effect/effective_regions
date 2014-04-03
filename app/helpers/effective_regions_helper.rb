80770efa53e42f5788129bf0d67735acc8419970
Matt Riemer <matthew@agilestyle.com>
2014-04-03 09:51:23 -0600

    Refactored helpers to build render_snippet ability

--- app/helpers/effective_regions_helper.rb
+++ app/helpers/effective_regions_helper.rb
@@ -16,7 +16,7 @@ module EffectiveRegionsHelper
 
   # Loads the Ckeditor Javascript & Stylesheets only when in edit mode
   def effective_regions_include_tags
-    if request.fullpath.include?('?edit=true')
+    if effectively_editting?
       javascript_include_tag('effective_ckeditor') + stylesheet_link_tag('effective_ckeditor')
     end
   end
@@ -26,85 +26,74 @@ module EffectiveRegionsHelper
   def ckeditor_region(args, options = {}, &block)
     obj = args.first
     title = args.last.to_s.parameterize
-    editting = request.fullpath.include?('?edit=true')
+
+    # Set up the editable div options we need to send to ckeditor
+    if effectively_editting?
+      opts = {
+        :contenteditable => true, 
+        'data-effective-ckeditor' => (options.delete(:type) || :full).to_s, 
+        :style => ['-webkit-user-modify: read-write;', options.delete(:style)].compact.join(' '),
+        :class => ['effective-region', options.delete(:class)].compact.join(' ')
+      }.merge(options)
+    end
 
     if obj.kind_of?(ActiveRecord::Base)
       raise StandardError.new('Object passed to effective_region helper must declare act_as_regionable') unless obj.respond_to?(:acts_as_regionable)
 
       region = obj.regions.find { |region| region.title == title }
-      content = region.try(:content)
 
-      if editting
+      if effectively_editting?
         can_edit = (EffectiveRegions.authorized?(controller, :update, obj) rescue false)
-
-        opts = {
-          :id => [model_name_from_record_or_class(obj).param_key(), obj.id, title].join('_'), 
-          :contenteditable => true, 
-          'data-effective-ckeditor' => (options.delete(:type) || :full).to_s, 
-          :style => ['-webkit-user-modify: read-write;', options.delete(:style)].compact.join(' '),
-          :class => ['effective-region', options.delete(:class)].compact.join(' ')
-        }.merge(options)
+        opts[:id] = [model_name_from_record_or_class(obj).param_key(), obj.id, title].join('_')
       end
-    else
+    else # This is a global region
       regions = (@effective_regions_global ||= Effective::Region.global.to_a)
       region = regions.find { |region| region.title == title } || Effective::Region.new(:title => title)
-      content = region.try(:content)
 
-      if editting
+      if effectively_editting?
         can_edit = (EffectiveRegions.authorized?(controller, :update, region) rescue false)
-
-        opts = {
-          :id => title.to_s.parameterize, 
-          :contenteditable => true, 
-          'data-effective-ckeditor' => (options.delete(:type) || :full).to_s, 
-          :style => ['-webkit-user-modify: read-write;', options.delete(:style)].compact.join(' '),
-          :class => ['effective-region', options.delete(:class)].compact.join(' ')
-        }.merge(options)
+        opts[:id] = title.to_s.parameterize
       end
     end
 
-    if editting && can_edit # If we need the editable div
+    if effectively_editting? && can_edit # If we need the editable div
       content_tag(:div, opts) do
-        content.present? ? expand_snippets(editable(content, region), region).html_safe : ((capture(&block).strip.html_safe) if block_given?)
+        region.try(:content).present? ? render_region(region, true) : ((capture(&block).strip.html_safe) if block_given?)
       end
     else
-      content.present? ? expand_snippets(content, region).html_safe : ((capture(&block).strip.html_safe) if block_given?)
+      region.try(:content).present? ? render_region(region, false) : ((capture(&block).strip.html_safe) if block_given?)
     end
   end
 
-  # We're finding [snippet_0] and expanding to
-  # <div class="text_field_tag_snippet">[snippet_0]</div>
-  def editable(html, region)
-    html.scan(/\[(snippet_\d+)\]/).flatten.uniq.each do |id|  # Finds snippet_1
-      snippet = region.snippet_objects.find { |snippet| snippet.id == id }
+  def render_region(region, can_edit = true)
+    return '' unless region
 
-      if snippet
-        editable_div = content_tag(:div, "[#{snippet.id}]", :class => "#{snippet.class_name}_snippet", :data => {'effective-snippet' => snippet.data}).html_safe
-        html.gsub!("[#{id}]", editable_div)
+    region.content.tap do |html|
+      html.scan(/\[(snippet_\d+)\]/).flatten.uniq.each do |id| # find snippet_1 and replace with snippet content
+        snippet = region.snippet_objects.find { |snippet| snippet.id == id }
+        html.gsub!("[#{id}]", render_snippet(snippet, can_edit)) if snippet
       end
-
-    end
-    html
+    end.html_safe
   end
 
-  def expand_snippets(html, region)
-    html.scan(/\[(snippet_\d+)\]/).flatten.uniq.each do |id| # find snippet_1 and insert snippet content
-      content = snippet_content(id, region)
-      html.gsub!("[#{id}]", content) if content
+  def render_snippet(snippet, can_edit = true)
+    return '' unless snippet
+
+    if Rails.env.production?
+      content = render(:partial => snippet.to_partial_path, :object => snippet) rescue ''
+    else
+      content = render(:partial => snippet.to_partial_path, :object => snippet)
     end
-    html
+
+    if effectively_editting? && can_edit
+      content_tag(:div, content, :class => "#{snippet.class_name}_snippet", :data => {'effective-snippet' => snippet.data})
+    else
+      content
+    end.html_safe
   end
 
-  def snippet_content(id, region)
-    snippet = (region.try(:snippet_objects) || []).find { |snippet| snippet.id == id }
-
-    if snippet
-      if Rails.env.production?
-        render(:partial => snippet.to_partial_path, :object => snippet) rescue ''
-      else
-        render(:partial => snippet.to_partial_path, :object => snippet)
-      end
-    end
+  def effectively_editting?
+    @effectively_editting ||= request.fullpath.include?('?edit=true')
   end
 
 end
