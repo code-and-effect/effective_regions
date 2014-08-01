a76041d5885a3e5e8765c225e2570802230c4c56
Matt Riemer <matthew@agilestyle.com>
2014-08-01 12:47:09 -0600

    Remove paste region

--- app/helpers/effective_regions_helper.rb
+++ app/helpers/effective_regions_helper.rb
@@ -14,11 +14,6 @@ module EffectiveRegionsHelper
     block_given? ? ckeditor_region(args, options) { yield } : ckeditor_region(args, options)
   end
 
-  def paste_effective_region(*args)
-    (options = args.extract_options!).merge!(:type => :paste)
-    block_given? ? ckeditor_region(args, options) { yield } : ckeditor_region(args, options)
-  end
-
   def wrapped_snippet_effective_region(*args)
     (options = args.extract_options!).merge!(:type => :wrapped_snippets)
     block_given? ? ckeditor_region(args, options) { yield } : ckeditor_region(args, options)
@@ -45,8 +40,8 @@ module EffectiveRegionsHelper
     # Set up the editable div options we need to send to ckeditor
     if effectively_editting?
       opts = {
-        :contenteditable => true, 
-        'data-effective-ckeditor' => (options.delete(:type) || :full).to_s, 
+        :contenteditable => true,
+        'data-effective-ckeditor' => (options.delete(:type) || :full).to_s,
         'data-allowed-snippets' => [options.delete(:snippets)].flatten.compact.to_json,
         :style => ['-webkit-user-modify: read-write;', options.delete(:style), ('display: inline;' if options.delete(:inline))].compact.join(' '),
         :class => ['effective-region', options.delete(:class)].compact.join(' ')
