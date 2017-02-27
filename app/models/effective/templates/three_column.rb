0cc0e9a93931283bdab3b82472dcd7cc45f19cbd
Matt Riemer <matthew@agilestyle.com>
2017-02-27 09:48:01 -0700

    Add two and three column templates

---
+++ app/models/effective/templates/three_column.rb
@@ -0,0 +1,9 @@
+module Effective
+  module Templates
+    class ThreeColumn < Template
+      def description
+        'Three Column Area'
+      end
+    end
+  end
+end
---
+++ app/models/effective/templates/two_column.rb
@@ -0,0 +1,9 @@
+module Effective
+  module Templates
+    class TwoColumn < Template
+      def description
+        'Two Column Area'
+      end
+    end
+  end
+end
